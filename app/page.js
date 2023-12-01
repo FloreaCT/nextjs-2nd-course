ac78adcea777cc87bae2b0fe73b88c9e13e9ffbe
Cristian Florea <floreact@gmail.com>
2023-12-01 10:37:31 +0000

    Learned about Link and useRouter

--- app/page.js
+++ app/page.js
@@ -1,5 +1,37 @@
-const Home = () => {
-  return <div>Home</div>;
-};
+"use client";
 
+import Link from "next/link";
+import { useRouter } from "next/navigation";
+
+const Home = () => {
+  const router = useRouter();
+
+  const handelClick = (page) => {
+    router.push(page);
+  };
+
+  return (
+    <section>
+      <h1> Use Router</h1>
+      <Link
+        className="border px-2 py-4 rounded-md bg-gray-200 hover:bg-gray-300"
+        href="/projects"
+      >
+        Go to Projects Page
+      </Link>
+      <button
+        className="border px-2 py-4 rounded-md bg-gray-200 hover:bg-gray-300"
+        onClick={() => handelClick("about")}
+      >
+        Go to About Page
+      </button>
+      <button
+        className="border px-2 py-4 rounded-md bg-gray-200 hover:bg-gray-300"
+        onClick={() => handelClick("login")}
+      >
+        Go to Login Page
+      </button>
+    </section>
+  );
+};
 export default Home;
